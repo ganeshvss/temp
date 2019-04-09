4ae32c608cccf0d73ec7eb3a6b9dfe96a86db42e
GANESH <43468976+ganeshvss@users.noreply.github.com>
2019-04-09 15:51:38 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,3 +3,4 @@ echo "Welcome to jenkins"
 echo "First commit"
 echo "Second Commit"
 echo "Third commit"
+aws cloudformation create-stack --stack-name myteststack --template-body file://role.yml --capabilities CAPABILITY_NAMED_IAM
