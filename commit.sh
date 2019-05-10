6a0c72dc2618614c7f9c082bc5e634a15d07f674
GANESH <43468976+ganeshvss@users.noreply.github.com>
2019-05-10 15:49:21 +0530

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,4 +3,4 @@ echo "Welcome to jenkins"
 echo "First commit"
 echo "Second Commit"
 echo "Third commit"
-aws cloudformation create-stack --stack-name myrolestack --template-body file://role.yml --capabilities CAPABILITY_NAMED_IAM
+echo "Fourth Commit"
